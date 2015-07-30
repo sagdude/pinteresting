f1f710934cfa974b5decd2bac2a269fb87301b92
Mike Bradbury <wordsmithmb@gmail.com>
2015-07-29 18:47:59 -0700

    Created Pintastic Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require jquery.turbolinks
 //= require turbolinks
 //= require masonry/jquery.masonry
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pins#index"
   get "about" => "pages#about" # creates about_path
+  get "pintastic" => "pages#pintastic" # creates pintastic_path
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
